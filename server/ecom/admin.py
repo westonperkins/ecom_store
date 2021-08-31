97bbc29908ffa6797bd15191c9c9a12674d485d0
westonperkins <westonperkins5@gmail.com>
2021-08-30 20:28:07 -0400

    models

--- server/ecom/admin.py
+++ server/ecom/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Category
+from .models import Garment
+
+admin.site.register(Category)
+admin.site.register(Garment)
--- server/ecom/models.py
+++ server/ecom/models.py
@@ -1,3 +1,41 @@
+from typing import DefaultDict
 from django.db import models
 
 # Create your models here.
+
+class Category(models.Model):
+    category = models.CharField(max_length=100)
+ 
+    def __str__(self):
+        return self.category
+
+class Garment(models.Model):
+    EXTRA_SMALL = 'XS'
+    SMALL = 'S'
+    MEDIUM = 'M'
+    LARGE = 'L'
+    EXTRA_LARGE = 'XL'
+    SIZE_CHOICES = [
+        (EXTRA_SMALL, 'xs',),
+        (SMALL, 's'),
+        (MEDIUM, 'm'),
+        (LARGE, 'l'),
+        (EXTRA_LARGE, 'xl')
+    ]
+    price = models.IntegerField
+    title = models.CharField(max_length=100)
+    collection = models.CharField(max_length=100)
+    description = models.CharField(max_length=500)
+    sizes = models.CharField(
+        max_length=2,
+        choices=SIZE_CHOICES,
+        default=SMALL
+    )
+    category = models.ForeignKey(
+        Category,
+        on_delete=models.CASCADE,
+        related_name="garments"
+    )
+
+    def __str__(self):
+        return self.title
