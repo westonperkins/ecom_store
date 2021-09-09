d71c04db6868a95bf65fc5128dc7cbb782e95e60
westonperkins <westonperkins5@gmail.com>
2021-09-09 17:25:12 -0400

    register, login, user, logout on the backend

--- server/accounts/urls.py
+++ server/accounts/urls.py
@@ -1,10 +1,12 @@
 from django import urls
 from django.urls import path, include
-from .api import RegistrationAPI, LoginApi
+from .api import RegistrationAPI, LoginApi, UserAPI
 from knox import views as knox_views
 
 urlpatterns = [
     path('api/auth', include('knox.urls')),
     path('api/auth/register', RegistrationAPI.as_view()),
-    path('api/auth/login', LoginApi.as_view()) 
+    path('api/auth/login', LoginApi.as_view()) ,
+    path('api/auth/user', UserAPI.as_view()) ,
+    path('api/auth/logout', knox_views.LogoutView.as_view(), name="knox_logout")
 ]
